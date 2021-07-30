7f1988ebb9118b1f0f34eca9d47c1ce2ccd10077
Enio Marcelo <eniomarcelo@gmail.com>
2021-07-30 14:27:57 -0400

    TOASTER mc_alert_toast_ MESSAGE POSITION BOTTOM RIGHT - 30/07/2021 - 14:25hs

--- public/js/macros_js.js
+++ public/js/macros_js.js
@@ -11,11 +11,12 @@ function mc_alert_toast_success(_message) {
     $(document).Toasts('create', {
         title: 'SUCESSO...',
         body: _message,
-        class: 'bg-success toasts-width margin-top-5 margin-right-5',
+        class: 'bg-success toasts-width margin-bottom-5 margin-right-5',
         autohide: true,
         delay: 2000,
         fade: true,
-        icon: 'fa fa-thumbs-up '
+        icon: 'fa fa-thumbs-up ',
+        position: 'bottomRight'
     });
 }
 
@@ -28,10 +29,11 @@ function mc_alert_toast_warning(_message) {
     $(document).Toasts('create', {
         title: 'ATENÇÃO...',
         body: _message,
-        class: 'bg-warning toasts-width margin-top-5 margin-right-5',
+        class: 'bg-warning toasts-width margin-bottom-5 margin-right-5',
         autohide: true,
         delay: 3000,
         fade: true,
-        icon: 'fas fa-exclamation-triangle '
+        icon: 'fas fa-exclamation-triangle ',
+        position: 'bottomRight'
     });
 }
